3ecb57422d755a5f0ce0ef143d810228776bd321
Imamberdiev Farrukh <imamb00@mail.ru>
2021-07-27 20:31:22 +0000

    185941 / Разработка блока 'Регистрация/авторизация' в шапке сайта

--- local/templates/roga_i_sila_main/header.php
+++ local/templates/roga_i_sila_main/header.php
@@ -35,26 +35,20 @@ $asset->addJs(SITE_DEFAULT_PATH . '/assets/js/script.js');
                         <img src="<?=SITE_DEFAULT_PATH?>/assets/images/logo.png" width="222" height="30" alt="">
                     </span>
                 </div>
-                <div>
-                    <ul class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
-                        <li>
-                            <a class="text-gray-500 hover:text-orange" href="authorized.html">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                </svg>
-                                Регистрация
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-gray-500 hover:text-orange" href="form.html">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-                                </svg>
-                                Авторизация
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+
+                <?php $APPLICATION->IncludeComponent(
+                    "bitrix:system.auth.form",
+                    "auth_form_header",
+                    Array(
+                        "COMPONENT_TEMPLATE" => "auth_form_header",
+                        "FORGOT_PASSWORD_URL" => "",
+                        "PROFILE" => "/personal/profile/",
+                        "PROFILE_URL" => "/personal/",
+                        "REGISTER_URL" => "/auth/",
+                        "SHOW_ERRORS" => "N"
+                    )
+                );?>
+                
             </div>
         </div>
         <div class="border-b">
--- test.php
+++ test.php
@@ -1,19 +1,16 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Рога и Сила - главная страница");
-?><?$APPLICATION->IncludeComponent("bitrix:menu", "menu_footer", Array(
-	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-		"DELAY" => "N",	// Откладывать выполнение шаблона меню
-		"MAX_LEVEL" => "1",	// Уровень вложенности меню
-		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
-			0 => "",
-		),
-		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
-		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-		"ROOT_MENU_TYPE" => "bottom",	// Тип меню для первого уровня
-		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form", 
+	"auth_form_header", 
+	array(
+		"FORGOT_PASSWORD_URL" => "",
+		"PROFILE_URL" => "/personal/",
+		"REGISTER_URL" => "/auth/",
+		"SHOW_ERRORS" => "N",
+		"COMPONENT_TEMPLATE" => "auth_form_header",
+		"PROFILE" => "/personal/profile/"
 	),
 	false
 );?>
